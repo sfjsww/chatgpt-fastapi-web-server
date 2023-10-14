c3ac7d6133d8c8cb7033d3283a079dd255d8c3dc
liuruozhuo <liuruozhuo@corp.netease.com>
2023-10-14 21:06:26 +0800

    feat: [gpt] adjust api

--- model/Response/chatgpt_response.py
+++ model/Response/chatgpt_response.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
 
 class ChatgptResponse(BaseModel):
+    success: bool
     msg: str
--- router/v1/__init__.py
+++ router/v1/__init__.py
@@ -14,7 +14,7 @@ API_KEY = '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'
 async def chatgpt(data: ChatgptRequest):
     try:
         res = chat_gpt.reply(data.text, Context(ContextType.TEXT, kwargs={'session_id': data.session_id, 'openai_api_key': API_KEY}))
-        return ChatgptResponse(msg=res.content)
+        return ChatgptResponse(success=True, msg=res.content)
     except Exception as e:
         print(e)
-        raise e
+        return ChatgptResponse(success=False, msg=str(e))
